0a3920a47cb967e58e79e437f63e20f6444d0506
Travis Johnson <TJcriss25@gmail.com>
2019-03-24 18:11:56 -0600

    Got basic grib for SW men and women showing

--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -1,7 +1,5 @@
 import { people } from '../data/people.js'
 
-
-
 const men = people.filter(person => person.gender === 'male')
 const women = people.filter(person => person.gender === 'female')
 const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'))
@@ -15,6 +13,19 @@ men.forEach((man) => {
     let manElement = document.createElement('div')
     manElement.className = 'box'
     manElement.textContent = man.name
+    let eyeColor = document.createElement('p')
+    eyeColor.textContent = man.eye_color
+    manElement.appendChild(eyeColor)
+    mainContainer.appendChild(manElement)
+})
+
+women.forEach((man) => {
+    let manElement = document.createElement('div')
+    manElement.className = 'box'
+    manElement.textContent = man.name
+    let eyeColor = document.createElement('p')
+    eyeColor.textContent = man.eye_color
+    manElement.appendChild(eyeColor)
     mainContainer.appendChild(manElement)
 })
 
